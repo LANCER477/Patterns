9a776ea7b98805ba31272702e3027265eaeab7b5
maksi <maksi@BOB>
2025-03-30 22:08:08 +0300

    com

---
+++ FabricMethod/FileName.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+
+
+class Transport {
+public:
+    virtual ~Transport() {}
+    virtual string Deliver() const = 0;
+};
+
+class Truck : public Transport {
+public:
+    string Deliver() const override {
+        return "Truck deliver!";
+    }
+};
+
+class Ship : public Transport {
+public:
+    string Deliver() const override {
+        return "Ship deliver!";
+    }
+};
+
+class Logistic {
+public:
+    virtual ~Logistic() {};
+    virtual Transport* FactoryMethod() const = 0;
+    string SomeOperation() const {
+        Transport* transport = this->FactoryMethod();
+        string result = "Logistic: created " + transport->Deliver();
+        delete transport;
+        return result;
+    }
+};
+
+class CreateTruck : public Logistic {
+public:
+    Transport* FactoryMethod() const override {
+        return new Truck();
+    }
+};
+
+class CreateShip : public Logistic {
+public:
+    Transport* FactoryMethod() const override {
+        return new Ship();
+    }
+};
+
+int main() {
+
+}
